5798a9568f323ea00d6e0ae9e9c2aa9c5278c5e3
alihacks <alihacks@pm.me>
2020-12-05 08:28:02 -0500

    Dec05 done, still set based

---
+++ Dec05/Dec05_part2.sql
@@ -0,0 +1,41 @@
+USE tempdb 
+GO
+-- Run input.sql first
+WITH Input AS (
+	SELECT
+		ROW_NUMBER() OVER (ORDER BY i.string) AS BoardingPassId,
+		i.string
+	FROM dbo.Dec05Input AS i
+	-- Testing -- (VALUES ('FBFBBFFRLR'), ('BFFFBBFRRR'), ('FFFBBBFRRR'), ('BBFFBBFRLL')) AS i (string)
+),
+Places AS (
+	SELECT
+		nums.num,
+		-- We only add to our valur if it's a Back or a Right
+		IIF(nums.num BETWEEN 1 AND 7, 'B', 'R') AS PlaceChar,
+		CASE
+			-- First 7 chars are front/backs
+			WHEN nums.num BETWEEN 1 AND 7 THEN
+				POWER(2, 7 - nums.num) * 8 -- the value will be like (64, 32, 16..) * 8
+			-- Rest is left/rights
+			ELSE
+				POWER(2, 10 - nums.num) -- and Rights seats add 4,2,1
+		END AS PlaceValue
+
+	-- This can be generated but for 10 I may just as well type it out
+	FROM (VALUES (1), (2), (3), (4), (5), (6), (7), (8), (9), (10)) AS nums (num)
+),
+SeatNumbers AS (
+	SELECT
+		Input.BoardingPassId,
+		SUM(Places.PlaceValue) AS SeatNumber
+	FROM Input
+	INNER JOIN Places
+		ON SUBSTRING(Input.string, Places.num, 1) = Places.PlaceChar
+	GROUP BY Input.BoardingPassId
+)
+--Only Change from part 1 is below
+SELECT MIN(SN1.SeatNumber) -- Doing Min because the higher number returned will be the last seat (922) which we know is not ours
+FROM SeatNumbers SN1
+LEFT JOIN SeatNumbers SN2 ON SN1.SeatNumber = SN2.SeatNumber - 1 -- Seat to the + 1 of "mine"
+WHERE SN2.SeatNumber IS NULL
