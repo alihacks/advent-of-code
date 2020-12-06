a5edf0d59f678a1b4df875d399e63510f77a3926
alihacks <alihacks@pm.me>
2020-12-06 09:25:10 -0500

    Dec06 done, lots of counting
    reformat

--- Dec06/Dec06_part1.sql
+++ Dec06/Dec06_part1.sql
@@ -3,31 +3,35 @@ GO
 -- Run Dec06Input.sql first to setup input. This can be inlined but moved to a single file to keep code cleaner
 WITH InputGroups AS (
 	SELECT
-		ROW_NUMBER() OVER (ORDER BY InputString) AS Groupid,
+		ROW_NUMBER() OVER (ORDER BY InputString) AS GroupId,
 		REPLACE(REPLACE(value, CHAR(0xD) + CHAR(0xA), ''), ' ', '') AS ItemString
 	FROM Dec06Input
 	CROSS APPLY STRING_SPLIT(REPLACE(InputString, CHAR(0xD) + CHAR(0xA) + CHAR(0xD) + CHAR(0xA), '^'), '^')
 ),
-MaxInputLen AS(
-SELECT MAX(LEN(ItemString)) AS MaxLen FROM InputGroups
-),
+MaxInputLen AS (SELECT MAX(LEN(InputGroups.ItemString)) AS MaxLen FROM InputGroups),
 -- Generate enough numbers for our needs
-Numbers AS (SELECT 1 AS num
-        UNION ALL
-        SELECT num + 1 
-        FROM Numbers
-        WHERE num <= (SELECT MaxLen FROM MaxInputLen)
-       ),
-GroupsWithLetters AS(
-SELECT GroupId, SUBSTRING(IG.ItemString,num,1) AS Letter
-FROM InputGroups AS IG
-INNER JOIN Numbers AS N ON N.num <= LEN(IG.ItemString)
+Numbers AS (
+	SELECT 1 AS num
+	UNION ALL
+	SELECT Numbers.num + 1
+	FROM Numbers
+	WHERE Numbers.num <= (SELECT MaxInputLen.MaxLen FROM MaxInputLen)
 ),
-GroupCounts AS(
-SELECT GroupId, COUNT(DISTINCT Letter) AS GroupLetterCount
-FROM GroupsWithLetters
-GROUP BY GroupId
+GroupsWithLetters AS (
+	SELECT
+		IG.GroupId,
+		SUBSTRING(IG.ItemString, N.num, 1) AS Letter
+	FROM InputGroups AS IG
+	INNER JOIN Numbers AS N
+		ON N.num <= LEN(IG.ItemString)
+),
+GroupCounts AS (
+	SELECT
+		GroupsWithLetters.GroupId,
+		COUNT(DISTINCT GroupsWithLetters.Letter) AS GroupLetterCount
+	FROM GroupsWithLetters
+	GROUP BY GroupsWithLetters.Groupid
 )
-SELECT SUM(GroupLetterCount) AS Answer
+SELECT SUM(GroupCounts.GroupLetterCount) AS Answer
 FROM GroupCounts
-OPTION(MAXRECURSION 300)
+OPTION (MAXRECURSION 300)
