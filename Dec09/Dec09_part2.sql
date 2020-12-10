fb1e20f94809e7d7c9fe4c2079387452d94e716b
alihacks <alihacks@pm.me>
2020-12-09 19:58:36 -0500

    Dec09 done, SQL Server being all slow so I had to add an indexed temp table

--- Dec09/Dec09_part2.sql
+++ Dec09/Dec09_part2.sql
@@ -5,8 +5,10 @@ GO
 -- This is a performance only hack as string split seems to be slow in a large set based operation
 -- We can work w/o this step but it's much faster this way
 DROP TABLE IF EXISTS #input
-CREATE TABLE #input(NumIndex INT, Num BIGINT, INDEX IX1 CLUSTERED(NumIndex, Num))
-INSERT #input SELECT * FROM dec09input;
+CREATE TABLE #input (NumIndex INT, Num BIGINT, INDEX IX1 CLUSTERED (NumIndex, Num))
+INSERT #input
+SELECT *
+FROM dec09input;
 
 WITH Part1Answer AS (SELECT 29221323 AS Answer), -- Pasted for efficiency, can use prior CTE,
 Ranges AS (
@@ -23,19 +25,22 @@ RunningSums AS (
 	SELECT *
 	FROM Ranges
 	CROSS APPLY (
-		SELECT SUM(num) AS RangeSum
+		SELECT SUM(Num) AS RangeSum
 		FROM #input
 		WHERE
 			NumIndex BETWEEN StartIndex AND EndIndex
 	) AS Sums
 ),
-AnswerIndexes AS(
-SELECT StartIndex, EndIndex
-FROM RunningSums
-WHERE RunningSums.RangeSum = (SELECT Part1Answer.Answer FROM Part1Answer)
+AnswerIndexes AS (
+	SELECT
+		RunningSums.StartIndex,
+		RunningSums.EndIndex
+	FROM RunningSums
+	WHERE RunningSums.RangeSum = (SELECT Part1Answer.Answer FROM Part1Answer)
 )
-SELECT MIN(Num) + MAX(Num) AS Answer
-FROM AnswerIndexes A
-CROSS APPLY #input I
-WHERE I.numindex BETWEEN A.StartIndex AND A.EndIndex
+SELECT MIN(I.Num) + MAX(I.Num) AS Answer
+FROM AnswerIndexes AS A
+CROSS APPLY #input AS I
+WHERE
+	I.NumIndex BETWEEN A.StartIndex AND A.EndIndex
 
