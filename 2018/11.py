4409ff1ec4e0cd4ae833d78a7be78faac45570a0
alihacks <alihacks@pm.me>
2021-01-01 19:54:28 -0500

    Refactor 2018 Dec11 to partial sums:

--- 2018/11.py
+++ 2018/11.py
@@ -15,21 +15,44 @@ def get_power(x, y, serial):
     return int(str(power)[-3]) - 5
 
 
-def get_max(grid, boxsize):
+def get_max(sums, boxsize):
     maxp = 0
     maxloc = None
     search_range = 300 - boxsize - 1
     for x0 in range(search_range):
         for y0 in range(search_range):
             x, y = x0 + 1, y0 + 1
-            totp = 0
-            for dx in range(boxsize):
-                for dy in range(boxsize):
-                    totp += grid[x0+dx][y0+dy]
+            totp = get_sum(sums, x0, y0, boxsize)
             if totp > maxp:
                 maxp = totp
                 maxloc = x, y
     return maxloc, maxp
+
+
+def calc_sums(grid):
+    n = len(grid)
+    sums = [[0 for _ in range(n)] for _ in range(n)]
+    sums[0] = copy.deepcopy(grid[0])
+    # cols
+    for x in range(1, n):
+        for y in range(0, n):
+            sums[x][y] = grid[x][y] + sums[x - 1][y]
+    # rows
+    for x in range(0, n):
+        for y in range(1, n):
+            sums[x][y] += sums[x][y - 1]
+    return sums
+
+
+def get_sum(sums, x, y, size):
+    x1 = x+size - 1
+    y1 = y+size - 1
+    res = sums[x1][y1]
+    res -= sums[x - 1][y1] if x > 0 else 0
+    res -= sums[x1][y - 1] if y > 0 else 0
+    if (x > 0 and y > 0):
+        res += sums[x - 1][y - 1]
+    return res
 
 
 def main(input, is_real):
@@ -39,16 +62,17 @@ def main(input, is_real):
     for x0, y0 in itertools.product(range(300), range(300)):
         x, y = x0 + 1, y0 + 1
         grid[x0][y0] = get_power(x, y, serial)
+    sums = calc_sums(grid)
 
-    ans, _ = get_max(grid, 3)
+    ans, _ = get_max(sums, 3)
     print("Part1:", ans)
 
     maxloc = None
     maxbox = 0
     maxpow = 0
-    for s in range(2, 15):
+    for s in range(1, 300):
         # print(s)
-        l, p = get_max(grid, s)
+        l, p = get_max(sums, s)
         if p > maxpow:
             maxpow = p
             maxloc = l
