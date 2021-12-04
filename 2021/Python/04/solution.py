5d8d0cd440caa318057a8f405f585f963278726c
alihacks <alihacks@pm.me>
2021-12-04 10:54:42 -0500

    main dec04 python cleanup

--- 2021/Python/04/solution.py
+++ 2021/Python/04/solution.py
@@ -2,8 +2,8 @@ from typing import List
 
 
 class BingoBoard:
-    def __init__(self, input_string, idx):
-        self.idx = idx
+    def __init__(self, input_string):
+        self.is_bingo = False
         # storing rows and cols separately to easily check for BINGO
         self.rows = [set() for _ in range(5)]
         self.cols = [set() for _ in range(5)]
@@ -16,13 +16,12 @@ class BingoBoard:
                 self.cols[c].add(nums[c])
 
     def play(self, num) -> bool:  # return true if BINGO
-        is_bingo = False
         for i in range(5):
             self.rows[i].discard(num)
             self.cols[i].discard(num)
             if len(self.rows[i]) == 0 or len(self.cols[i]) == 0:
-                is_bingo = True
-        return is_bingo
+                self.is_bingo = True
+        return self.is_bingo
 
     def get_sum(self) -> int:
         return sum([sum(self.rows[i]) for i in range(5)])
@@ -33,10 +32,8 @@ def parse(instr: str):
     parts = instr.split("\n\n")
     nums = list(map(int, parts[0].split(",")))
     board_input = parts[1::]
-    b_idx = 1
     for bi in board_input:
-        boards.append(BingoBoard(bi, b_idx))
-        b_idx = b_idx + 1
+        boards.append(BingoBoard(bi))
     return nums, boards
 
 
@@ -51,16 +48,12 @@ def partOne(instr: str) -> int:
 
 def partTwo(instr: str) -> int:
     nums, boards = parse(instr)
-    winners = set()
-    last_winner = 0
+    last_winner = None
     for num in nums:
-        if len(winners) == len(boards):
-            break
         for board in boards:
-            if board.idx in winners:
+            if board.is_bingo:
                 continue
             is_bingo = board.play(num)
             if is_bingo:
-                winners.add(board.idx)
                 last_winner = board.get_sum() * num
     return last_winner
