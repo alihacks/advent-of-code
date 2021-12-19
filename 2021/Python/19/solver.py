3c7396d42589951c7b15d1de62dd27339f5d5b6d
alihacks <alihacks@pm.me>
2021-12-19 18:26:33 -0500

    dec19 python cleanup

--- 2021/Python/19/solver.py
+++ 2021/Python/19/solver.py
@@ -7,25 +7,15 @@ class Scanner:
     def __init__(self, points):
         self.points = points
         self.perms = []
-
-        # 6 x,y,z swaps
+        # pre-compute all permutations, start with x,y,z switching
         for rots in permutations(range(3)):
-            # each can be + or -
+            # add sign change: + or -
             for signs in product([-1, 1], repeat=3):
                 new_points = []
                 for point in points:
-                    # TODO refac
-                    new_point = [
-                        signs[0] * point[rots[0]],
-                        signs[1] * point[rots[1]],
-                        signs[2] * point[rots[2]],
-                    ]
-                    # print(new_point)
+                    new_point = [signs[i] * point[rots[i]] for i in range(3)]
                     new_points.append(new_point)
                 self.perms.append(new_points)
-
-        # print(self.perms)
-        # print(len(self.perms))
 
 
 class Solver:
@@ -34,8 +24,6 @@ class Solver:
         self.is_test = is_test
         self.test1 = 79
         self.test2 = 3621
-        self.part1 = 0
-        self.part2 = 0
 
     def parse(self, instr) -> List:
         self.scanners = []
@@ -46,69 +34,56 @@ class Solver:
                 points.append([int(i) for i in coord.split(",")])
             self.scanners.append(Scanner(points))
 
-    def overlaps(self, grid, offsets, perm):
-        # print("ingrid", len(grid))
-        for i in range(len(grid)):
-            existing_scanner = grid[i]
+    def overlaps(self, perm):
+        for existing_scanner, (m_x, m_y, m_z) in self.matched:
             deltas = defaultdict(int)
-            # print("check")
             for x, y, z in perm:
                 for ex, ey, ez in existing_scanner:
                     dx, dy, dz = ex - x, ey - y, ez - z
                     deltas[(dx, dy, dz)] += 1
-            # print(deltas)
+
             for (dx, dy, dz), val in deltas.items():
                 if val >= 12:
-                    my_x, my_y, my_z = offsets[i]
-                    return [dx + my_x, dy + my_y, dz + my_z]
+                    return [dx + m_x, dy + m_y, dz + m_z]
         return None
 
-    def match_scanners(self, grid, offsets, seen):
-        print("Matching with seen=", seen)
-        if len(grid) == len(self.scanners):
-            return grid
+    def match_scanners(self):
+        print("Matching with seen=", self.seen)
+        if len(self.matched) == len(self.scanners):
+            return self.matched
         for i in range(len(self.scanners)):
-            if i in seen:
+            if i in self.seen:
                 continue
-            seen.add(i)
+            self.seen.add(i)
             for perm in self.scanners[i].perms:
                 # print("perm")
-                deltas = self.overlaps(grid, offsets, perm)
+                deltas = self.overlaps(perm)
                 if deltas is not None:
                     print(i, "fits")
-                    grid.append(perm)
-                    offsets.append(deltas)
-                    sol = self.match_scanners(grid, offsets, seen)
+                    self.matched.append([perm, deltas])
+                    sol = self.match_scanners()
                     if sol is not None:
                         return sol
-                    grid.pop()
-                    offsets.pop()
-            seen.remove(i)
+                    self.matched.pop()
+            self.seen.remove(i)
 
         return None
 
     def solve(self):
-        self.part1 = 0
-
-        grid = [self.scanners[0].points]
-        offsets = [[0, 0, 0]]
-        seen = set([0])
-        matched = self.match_scanners(grid, offsets, seen)
-        # corrected = []
+        # first scanner is 0,0,0 as-is
+        self.matched = [[self.scanners[0].points, [0, 0, 0]]]
+        self.seen = set([0])
+        self.match_scanners()
         beacons = set()
-        for i in range(len(matched)):
-            dx, dy, dz = offsets[i]
-            # print(dx, dy, dz)
-            for point in matched[i]:
-                mx, my, mz = point
+        for points, (dx, dy, dz) in self.matched:
+            for mx, my, mz in points:
                 beacons.add(tuple([mx + dx, my + dy, mz + dz]))
 
         self.part1 = len(beacons)
-
         self.part2 = 0
 
-        for b1 in offsets:
-            for b2 in offsets:
-                md = abs(b1[0] - b2[0]) + abs(b1[1] - b2[1]) + abs(b1[2] - b2[2])
+        for _, (x1, y1, z1) in self.matched:
+            for _, (x2, y2, z2) in self.matched:
+                md = abs(x1 - x2) + abs(y1 - y2) + abs(z1 - z2)
                 if md > self.part2:
                     self.part2 = md
