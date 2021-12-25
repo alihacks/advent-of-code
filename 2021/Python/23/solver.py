b816dce606964e9c66c31f0e4127e00d1f704f2d
alihacks <alihacks@pm.me>
2021-12-25 10:44:27 -0500

    dec23 python cleanup

--- 2021/Python/23/solver.py
+++ 2021/Python/23/solver.py
@@ -6,22 +6,25 @@ class Game:
     costs = {"A": 1, "B": 10, "C": 100, "D": 1000}
     cols = {"A": 0, "B": 1, "C": 2, "D": 3}
 
-    def indices():
+    def indices(self):
         res = []
         for i in range(11):
             res.append([0, i])
-        for i in [1, 2]:
+        for i in range(1, len(self.g)):
             for j in range(4):
                 res.append([i, j])
         return res
 
     def __init__(self, state):
         self.g = [list(state[0:11])]
-        self.g += [list(state[11:15]), list(state[15:19])]
+        i = 11
+        while i < len(state):
+            self.g.append(list(state[i : i + 4]))
+            i += 4
 
     def __str__(self):
         res = "".join(self.g[0]) + "\n"
-        for i in [1, 2]:
+        for i in range(1, len(self.g)):
             res += " |" + "|".join(self.g[i]) + "|\n"
         return res
 
@@ -31,28 +34,37 @@ class Game:
             state += "".join(part)
         return state
 
+    def room_ok(self, room):
+        return all(
+            [
+                i == "." or Game.cols[i] == room
+                for i in [self.g[j][room] for j in range(1, len(self.g))]
+            ]
+        )
+
     def moves(self, r, c):
         hallway_start = r == 0
         v = self.g[r][c]
         cr, cc = r, c
         m = []
-        if v == ".":
-            return []
         cost = Game.costs[v]
         current_cost = 0
 
-        if r == 2 and Game.cols[v] == cc:
-            return []
-        if r == 1 and Game.cols[v] == cc and self.g[r + 1][c] == v:
+        # in correct room, stay
+        if r > 0 and Game.cols[v] == cc and self.room_ok(c):
             return []
 
-        if cr == 2:
+        # get up in room
+        while cr > 1:
+            # if cr == 2:
             if self.g[r - 1][c] != ".":
                 # print("stuck")
                 return []
-            cr = r - 1
+            cr -= 1
             current_cost += cost
-            m.append([cr, cc, current_cost])
+            # m.append([cr, cc, current_cost])
+
+        # into hallway
         if cr == 1:
             if self.g[cr - 1][cc * 2 + 2] != ".":
                 # print("stuck")
@@ -76,22 +88,20 @@ class Game:
                 break
             m.append([cr, cc + rmove, current_cost + rmove * cost])
 
-        # Down moves from 0
-        # print(m)
+        # Down moves from hallway
         for r0, c0, cst in m:
             if r0 == 0 and c0 in [2, 4, 6, 8]:
                 new_c = (c0 - 2) // 2
-                if Game.cols[v] == new_c:
-                    # print(str(self))
-                    # print("try down from", r0, c0, cst, "to", new_c)
-
-                    if self.g[1][new_c] == "." and self.g[2][new_c] == ".":
-                        # print("move down two in col", new_c)
-                        m.append([2, new_c, cst + cost * 2])
-
-                    elif self.g[1][new_c] == "." and self.g[2][new_c] == v:
-                        # print("move down one in col", new_c)
-                        m.append([1, new_c, cst + cost])
+                if Game.cols[v] == new_c and self.room_ok(new_c):
+                    rd = r0
+                    # move down all the way you can
+                    while rd + 1 < len(self.g):
+                        if self.g[rd + 1][new_c] == ".":
+                            rd += 1
+                        else:
+                            break
+                    if rd != r0:
+                        m.append([rd, new_c, cst + cost * rd])
 
         res = []
         for r0, c0, cst in m:
@@ -103,7 +113,7 @@ class Game:
 
     def next_games(self):
         res = []
-        for r, c in Game.indices():
+        for r, c in self.indices():
             if self.g[r][c] == ".":
                 continue
             for new_r, new_c, new_cost in self.moves(r, c):
@@ -119,9 +129,7 @@ class Solver:
         self.parse(input)
         self.is_test = is_test
         self.test1 = 12521
-        self.test2 = 0
-        self.part1 = 0
-        self.part2 = 0
+        self.test2 = 44169
 
     def parse(self, instr):
         lines = instr.splitlines()
@@ -130,9 +138,7 @@ class Solver:
             state += lines[i].replace("#", "").strip()
         self.init = Game(state)
 
-    def solve(self):
-        self.part1 = 0
-
+    def least_energy(self):
         costs = {}
         q = [(0, self.init.get_state())]
 
@@ -143,7 +149,6 @@ class Solver:
             for next_game, next_cost in game.next_games():
                 next_state = next_game.get_state()
                 total_cost = cost + next_cost
-                # print(str(Game(next_state)))
                 if next_state in costs:
                     if total_cost < costs[next_state]:
                         costs[next_state] = total_cost
@@ -153,11 +158,11 @@ class Solver:
                     heapq.heappush(q, (total_cost, next_state))
 
         for key in costs:
-            if key.endswith("ABCDABCD"):
-                # print(key)
-                self.part1 = costs[key]
+            if key.startswith("." * 11):
+                return costs[key]
 
-
-# 17261 hi
-# 15261 low
-# 15299 YES
+    def solve(self):
+        self.part1 = self.least_energy()
+        state = self.init.get_state()
+        self.init = Game(state[:15] + "DCBADBAC" + state[15:])
+        self.part2 = self.least_energy()
