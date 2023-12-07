f8f9e8e1e5ec172a31ae565c12106fbf91b687cc
alihacks <alihacks@pm.me>
2023-12-07 10:23:47 -0500

    2023 dec06 add math

--- 2023/Python/06/solver.py
+++ 2023/Python/06/solver.py
@@ -1,5 +1,8 @@
 from typing import List
 import re, math
+from sympy import symbols, solve_univariate_inequality, S
+
+
 
 class Solver:
     def __init__(self, input_str, is_test: bool):
@@ -16,11 +19,16 @@ class Solver:
         
     def solve(self):
         self.part1 = 1
+
         def wins(time, dist):
             return len([n for n in range(time) if (time - n) * n > dist])
+        
+        def wins2(time, dist):
+            n = symbols('n')
+            return len(solve_univariate_inequality((time - n) * n > dist, n, relational=False).intersect(S.Integers))
 
         for time, dist in self.data:
-            self.part1 *= wins(time,dist)
+            self.part1 *= wins2(time,dist)
 
         time, dist = self.data2
-        self.part2 = wins(time,dist)
+        self.part2 = wins2(time,dist)
